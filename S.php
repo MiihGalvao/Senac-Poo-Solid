212dc299cc3e6d8ae7fdca8fa743c864337afb33
Milena Galvão <milena.galvao@itboxonline.com.br>
2021-05-04 21:56:38 -0300

    Criação do SOLID poo
    Criação dos arquivos SOLID, falta complementar

---
+++ S.php
@@ -0,0 +1,22 @@
+<?php 
+//SOLID
+//Single Responsibility
+
+class Usuario{
+
+    private $nome;
+
+    public funtion setNome()
+    {
+
+    }
+    public function getNome()
+    {
+
+    }
+    public function enviarEmail(){
+
+
+    }
+
+}
