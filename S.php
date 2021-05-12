c21b45af5ca16fc05d984a791644d18f54f293f2
Milena Galvão <milena.galvao@itboxonline.com.br>
2021-05-11 21:14:59 -0300

    Inicio da implementação da classe Usuario no SOLID
    Inicio da implementação da classe Usuario no SOLID - S.php

--- S.php
+++ S.php
@@ -8,15 +8,43 @@ class Usuario{
 
     public funtion setNome()
     {
-
+            //implementação
     }
     public function getNome()
     {
-
+             //implementação
     }
-    public function enviarEmail(){
-
-
+    public function enviarEmail()
+    {
+              //implementação
+    }
+    public function exportarParaPlanilha() //Ferindo o principio de responsabilidade unica.
+    {
+             //implementação
     }
 
+    class Usuario {
+        private $nome;
+
+        public function setNome()
+        {
+            //implementação
+        }
+        public function getNome()
+        {
+            //implementação
+        }
+    }
+        class email {
+
+            public function enviarEmail()
+            {
+                //implementação
+            }
+        }
+        class ExportarDados
+        {
+
+        }
+        class exportarParaPlanilha extends
 }
