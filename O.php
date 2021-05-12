992b15059168e8715f14ce55fda7e37e5451f290
Milena Galvão <milena.galvao@itboxonline.com.br>
2021-05-11 21:40:37 -0300

    3ºExemplo de OCP
    Fiz o 3º exemplo de OCP com as classes negativação, e interface Orgao Negativador.

--- O.php
+++ O.php
@@ -46,8 +46,110 @@ interface EmpresaDeLogistica {
     class Frete {
 private $empresa;
 
-public function construct (EmpresaDeLogistica $empresa){
-    
+    public function construct (EmpresaDeLogistica $empresa) 
+        {
+            $this->empresa = $empresa;
+        }
+    public function calcular()
+        {
+             //Logica com os metodos definidos na interface
+        }
 }
+//3ª EXEMPLO DE OCP ( OPEN CLOSE PRINCIPLE)
 
+class Negativação{
+
+    private $devedor;
+
+    public function__construct(Devedor $devedor)
+    {
+            $this->devedor = $devedor;
     }
+    public function enviar(string $orgaoNegativador)
+    {
+        if ($orgaoNegativador == 'Serasa'){
+            //Lógica
+        }
+        else if ($orgaoNegativador == 'SPC Brasil') {
+            //Lógica
+        }
+        else 
+        {
+            return 'Erro, órgão negativador não suportado';
+        }
+    }
+}
+//Refatorando para atendermos ao OCP
+
+interface OrgaoNegativador
+    {
+    public function enviarRemessaInclusao();
+    public function enviarRemessaExclusao();
+    public function receberRetorno();
+    }
+
+    class Serasa implements OrgaoNegativador {
+        public funtion enviarRemessaInclusao()
+        {
+            //Lógica
+        }
+        public function enviarRemessaExclusao()
+        {
+            //Lógica
+        }
+        public function receberRetorno()
+        {
+            //Lógica
+        }
+    }
+    class SPCBrasil implements OrgaoNegativador {
+        public funtion enviarRemessaInclusao()
+        {
+            //Lógica
+        }
+        public function enviarRemessaExclusao()
+        {
+            //Lógica
+        }
+        public function receberRetorno()
+        {
+            //Lógica
+        }
+    }
+    class BoaVista implements OrgaoNegativador {
+        public funtion enviarRemessaInclusao()
+        {
+            //Lógica
+        }
+        public function enviarRemessaExclusao()
+        {
+            //Lógica
+        }
+        public function receberRetorno()
+        {
+            //Lógica
+        }
+    }
+    class Negativacao {
+        private $devedor;
+
+        public function__construct(Devedor $devedor)
+        {
+            this->devedor = $devedor;
+        }
+        public function enviar (OrgaoNegativador $orgao)
+        {
+            //Lógica
+        }
+    }
+   /*     class Main {
+            public function restricao()
+            {
+                $negativacao = new Negativacao;
+
+                $negativacao->enviar($orgao);
+            }
+        }
+    */
+
+    
