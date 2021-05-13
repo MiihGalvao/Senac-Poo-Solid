08dd197a0f0b4e957b75eb3f6e92f87d51bd4929
Milena Galvão <milena.galvao@itboxonline.com.br>
2021-05-13 20:45:15 -0300

    Finalizei todas as classes e funcoes da S - Solid
    Finalizei todas as classes e funcoes da S - Solid

--- S.php
+++ S.php
@@ -7,44 +7,164 @@ class Usuario{
     private $nome;
 
     public funtion setNome()
-    {
+        {
             //implementação
-    }
+        }
     public function getNome()
-    {
+        {
              //implementação
-    }
+        }
     public function enviarEmail()
-    {
+        {
               //implementação
-    }
+        }
     public function exportarParaPlanilha() //Ferindo o principio de responsabilidade unica.
-    {
+        {
              //implementação
-    }
+        }
+}
 
+//Refatorar sem ferir o principio de responsabilidade única
     class Usuario {
+
         private $nome;
 
         public function setNome()
-        {
+            {
             //implementação
-        }
+            }
         public function getNome()
-        {
+            {
             //implementação
-        }
+            }
     }
         class email {
-
             public function enviarEmail()
-            {
+                {
                 //implementação
-            }
-        }
+                }
+    }
         class ExportarDados
+            {
+                //Implementação
+            }
+        class exportarDadosPlanilha extends ExportaDados
+               {
+
+            public function exportarParaPlanilha()
+                {
+                //Implementação
+                }
+        }
+//FIM DO EXEMPLO 1
+
+class Relatorio{
+
+    private $dados;
+
+    public  function setDados(array $dados)
         {
 
         }
-        class exportarParaPlanilha extends
+    public function exportarParaPdf() // Ferindo o principio de responsabilidade unica.
+        {
+        //Implementação
+        }
+    public function exportarParaCsv() // Ferindo o principio de responsabilidade unica.
+        {
+        //Implementação
+        }
 }
+//Refatorar sem ferir o principio de responsabilidade unica.
+
+class Relatorio {
+
+    private $dados;
+
+    public function setDados(array $dados)
+        {
+
+        }   
+}
+
+class ExportaDados
+        {
+    //Implementação
+        }
+
+class exportarDadosPdf extends ExportaDados{
+
+    public function exportar()
+        {
+    //Implementação
+        }
+}
+
+class ExportaDadosCsv extends ExportaDados
+        {
+
+        public function exportar()
+            {
+            //Implementação 
+            }
+}
+
+
+//FIM DO EXEMPLO 2
+
+//EXEMPLO III
+ 
+class Post{
+
+    private $titulo;
+
+    public function setTitulo(string $titulo)
+        {
+        //Implementação
+        }
+    public function gerarUrlAmigavel() //Ferindo o principio de responsabilidade unica.
+        {
+           //Implementação
+        }
+    public function buscarPostSemelhante() //Ferindo o principio de responsabilidade unica.
+    {
+        //Implementação
+    }
+}
+
+//Refatorar sem ferir o principio de resposabilidade única.
+
+class Post {
+
+    private $titulo;
+
+    public function setTitulo(string $titulo)
+        {
+        //Implementação
+        }
+}
+
+class Url {
+
+    public function gerarUrlAmigavel()
+        {
+            //Implementação
+        }
+}
+
+class Busca 
+    {
+        //Implementação
+    }
+class BuscaPorSemelhanca extends Busca {
+
+    public function buscar()
+        {
+            //  Implementação
+        }
+
+}
+
+
+//FIM DO EXEMPLO III
+
